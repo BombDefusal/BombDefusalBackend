c7cb7d3a7a676cd3467fd98e4026b79665bfa7a1
LautiGR_116 <lreinoso@mail.austral.edu.ar>
2024-06-03 10:50:25 -0300

    cosas

--- index.js
+++ index.js
@@ -1,58 +1,61 @@
-    const express = require('express');
-    const app = express();
-    const path = require('path');
-    const session = require('express-session');
-    const mongoose = require('mongoose');
-    const db = require('../BombDefusalBackend/src/main/backend/model/index')
+const express = require('express');
+const app = express();
+const path = require('path');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const db = require('../BombDefusalBackend/src/main/backend/model/index');
+const leaderboards = require('./src/main/backend/route/leaderboard');
 
-    const leaderboards = require('./src/main/backend/route/leaderboard')
+app.use(cors());
+app.use(express.json());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        sameSite: 'strict',
+    }
+}));
 
-    app.use(express.json());
-    app.use(session({
-        secret: 'secret',
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            sameSite: 'strict',
-        }
-    }));
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
 
-    app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use('/leaderboard', leaderboards);
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
-    app.use('/leaderboard', leaderboards);
+const Leaderboard = require('./src/main/backend/model/leaderboard');
 
-    app.use('/static', express.static(path.join(__dirname, 'public')));
+app.post('/leaderboard', async (req, res) => {
+    try {
+        await Leaderboard.create({
+            dispositive: req.body.dispositive,
+            time: req.body.time
+        });
+        return res.json("User created successfully");
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json(error);
+    }
+});
 
-    const Leaderboard = require('./src/main/backend/model/leaderboard')
+app.get('/leaderboard', async (req, res) => {
+    try {
+        const data = await Leaderboard.find({});
+        return res.json(data);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+});
 
-    app.post('/leaderboard', async (req, res) => {
-        try {
-            await Leaderboard.create({
-                dispositive: req.body.dispositive,
-                time: req.body.time
-            });
-            return res.json("User created successfully");
-        }
-        catch (error) {
-            console.error(error);
-            return res.status(400).json(error);
-        }
-    });
+mongoose.connect('mongodb://172.31.29.151:27017/bombdefusal')
+    .then(() => console.log('Connection to MongoDB established successfully.'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
 
-    app.get('/leaderboard', async (req, res) => {
-        try {
-            const data = await Leaderboard.find({});
-            return res.json(data);
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({ message: "Internal server error" });
-        }
-    });
-
-    mongoose.connect('mongodb://172.31.29.151:27017/bombdefusal')
-        .then(() => console.log('Connection to MongoDB established successfully.'))
-        .catch(err => console.error('Error connecting to MongoDB:', err));
-
-    app.listen(8081, () => {
-        console.log('Server is running on port 8081');
-    });
+app.listen(8081, () => {
+    console.log('Server is running on port 8081');
+});
